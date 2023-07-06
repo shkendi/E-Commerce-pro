7c4f0dce9eaf8417037a3aa44500b71bc6ff66cd
Shkendi <nshkendi@gmail.com>
2023-07-06 21:10:41 +0100

    first comment

---
+++ frontend/README.md
@@ -0,0 +1,5 @@
+# E-Commerce-Pro
+
+## Install tools
+1. Create Typescript React AQpp by Vite
+2. Create Componenets folder, Pages folder and Type folder
---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+			<header>e-Commerce</header>
+			<main></main>
+			<footer>All right reserved</footer>
+		</div>
+  )
+}
+
+export default App
